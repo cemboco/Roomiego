4cdc9a4932b2b3febf7442caa5a78e406ccfec11
Cem04 <45462437+cemboco@users.noreply.github.com>
2024-10-22 23:33:36 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      bufferutil: false,
+      "utf-8-validate": false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
