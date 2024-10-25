abd896caaf551036dc273e792c84722157df4053
Cem04 <45462437+cemboco@users.noreply.github.com>
2024-10-25 20:19:51 +0200

    Create constants.ts

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_SHOPPING_ITEMS = [
+  "Milch", "Brot", "Butter", "Eier", "Käse", "Joghurt",
+  "Tomaten", "Gurken", "Salat", "Äpfel", "Bananen",
+  "Nudeln", "Reis", "Kartoffeln", "Zwiebeln", "Knoblauch"
+]
